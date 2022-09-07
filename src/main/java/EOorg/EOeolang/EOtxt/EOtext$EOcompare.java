89582de3d36fa4e369f8dce50e22474afdf200f5
EugeneDar <evgenedar@mail.ru>
2022-09-07 13:30:05 +0300

    java used

---
+++ src/main/java/EOorg/EOeolang/EOtxt/EOtext$EOcompare.java
@@ -0,0 +1,77 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016-2022 Objectionary.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/**
+ * EO org.eolang.txt package.
+ *
+ * @checkstyle PackageNameCheck (4 lines)
+ * @since 0.0
+ */
+package EOorg.EOeolang.EOtxt;
+
+import org.eolang.AtComposite;
+import org.eolang.AtFree;
+import org.eolang.Data;
+import org.eolang.Dataized;
+import org.eolang.Param;
+import org.eolang.PhDefault;
+import org.eolang.Phi;
+import org.eolang.XmirObject;
+
+/**
+ * COMPARE.
+ *
+ * @checkstyle TypeNameCheck (5 lines)
+ * @since 0.0
+ */
+@XmirObject(oname = "text.compare")
+public class EOtext$EOcompare extends PhDefault {
+
+    /**
+     * Ctor.
+     *
+     * @param sigma Sigma
+     * @todo #18:30min Current implementation using Java.
+     *  We should implement it only via EOLANG code. We can do
+     *  it by using reduce method
+     */
+    public EOtext$EOcompare(final Phi sigma) {
+        super(sigma);
+        this.add("other", new AtFree());
+        this.add(
+            "φ",
+            new AtComposite(
+                this,
+                rho -> {
+                    final String other = new Dataized(rho.attr("other").get())
+                        .take(String.class);
+                    final Phi text = rho.attr("ρ").get();
+                    final String content = new Param(text, "s").strong(String.class);
+                    return new Data.ToPhi(content.compareTo(other));
+                }
+            )
+        );
+    }
+
+}
