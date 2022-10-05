487a64a40489319124e10e9728557a36cbe4a9b5
EugeneDar <evgenedar@mail.ru>
2022-10-05 20:03:57 +0300

    spaces added

--- src/main/java/EOorg/EOeolang/EOtxt/EOtext$EOreplaced.java
+++ src/main/java/EOorg/EOeolang/EOtxt/EOtext$EOreplaced.java
@@ -54,8 +54,8 @@ public class EOtext$EOreplaced extends PhDefault {
      *
      * @param sigma Sigma
      * @todo #21:30min Current implementation using Java.
-     * We should implement it only via EOLANG code. We can do
-     * it by using reduce method
+     *  We should implement it only via EOLANG code. We can do
+     *  it by using reduce method
      */
     public EOtext$EOreplaced(final Phi sigma) {
         super(sigma);
